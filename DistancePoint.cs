97624dab5092abbbfab4cab4d607720e8c92d283
pabloks <pabloks@a0305a6a-a706-11de-b195-ab392a2a73f5>
2009-11-23 04:18:06 +0000

    Triangulacion para encontrar las coordenadas del device. Hay que ver que pasa por que cuando pongo que la busque a veces se traba.

---
+++ DistancePoint.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+public class DistancePoint {
+
+    Vector3 point;
+    float distance;
+
+    public DistancePoint(Vector3 p, float d)
+    {
+        point = p;
+        distance = d;
+    }
+
+    public Vector3 getPoint()
+    {
+        return point;
+    }
+
+    public float getDistance()
+    {
+        return distance;
+    }
+
+    public override string ToString()
+    {
+        return point + "(" + distance + ")";
+    }
+
+    public void setPoint(Vector3 p)
+    {
+        point = p;
+    }
+
+    public void setDistance(float d)
+    {
+        distance = d;
+    }
+
+    public override bool Equals(object obj)
+    {
+        //hacer bien el chekeo de tipo
+        return (point.Equals(((DistancePoint)obj).getPoint())) && (distance.Equals(((DistancePoint)obj).getDistance()));
+    }
+}
